f804c49ab69f89e8ba318d88d41896bbf51cec95
Juan Herrera <102929112+1juanherrera@users.noreply.github.com>
2023-10-31 11:46:44 -0500

    update pbtec.js

--- pbtec.js
+++ pbtec.js
@@ -1,19 +1,26 @@
+//Todas las funciones deben recibir datos dinamicos
+/*
+Cacular promedio de estatura
+catidad de mujeres y hombres
+Asignar cada calificacion a su respectivo estudiante por id
+*/
+
 const students = [
-    { id: 1, name: "juan", age: 17, height: 1.60, gender: 'M' },
-    { id: 4, name: "pedro", age: 15, height: 1.90, gender: 'M' },
-    { id: 2, name: "ana", age: 16, height: 1.65, gender: 'F' },
-    { id: 3, name: "sofia", age: 17, height: 1.72, gender: 'F' },
-    { id: 6, name: "pedro", age: 12, height: 1.70, gender: 'M' },
-    { id: 5, name: "rosa", age: 19, height: 1.67, gender: 'F' },
-  ];
-  
+  { id: 1, name: "juan", age: 17, height: 1.60, gender: 'M' },
+  { id: 4, name: "pedro", age: 15, height: 1.90, gender: 'M' },
+  { id: 2, name: "ana", age: 16, height: 1.65, gender: 'F' },
+  { id: 3, name: "sofia", age: 17, height: 1.72, gender: 'F' },
+  { id: 6, name: "pedro", age: 12, height: 1.70, gender: 'M' },
+  { id: 5, name: "rosa", age: 19, height: 1.67, gender: 'F' },
+];
+
 const califications = [
-    { id: 1, calification: 5 },
-    { id: 4, calification: 5 },
-    { id: 3, calification: 5 },
-    { id: 2, calification: 1 },
-    { id: 5, calification: 3 },
-    { id: 6, calification: 4 },
+  { id: 1, calification: 5 },
+  { id: 4, calification: 5 },
+  { id: 3, calification: 5 },
+  { id: 2, calification: 1 },
+  { id: 5, calification: 3 },
+  { id: 6, calification: 4 },
 ]
 
 const media = ( average ) => {
@@ -26,40 +33,27 @@ const media = ( average ) => {
 console.log(media(students));
 
 const filterToGender = (genderList) => {
-    let mCount = 0;
-    let fCount = 0;
+    const maleGender = genderList.filter((person) => person.gender === 'M').length
+    const femaleGender = genderList.filter((person) => person.gender === 'F').length
 
-    for (const student of genderList) {
-    if (student.gender === 'M') {
-    mCount++;
-    } else if (student.gender === 'F') {
-    fCount++;
-    }
+    return `Female: ${femaleGender} - Male: ${maleGender}`
 }
 
-console.log(`Male ${mCount}`);
-console.log(`Female: ${fCount}`);
+console.log(filterToGender(students))
 
+const updatedStudents = [];
+
+const qualificationList = (totalStudents, qualifications) => {
+
+    totalStudents.forEach((student) => {
+    const calification = qualifications.find((cal) => cal.id === student.id);
+    const updatedStudent = { ...student, calification: calification.calification };
+    updatedStudents.push(updatedStudent);
+  });
+  return updatedStudents
 }
 
-filterToGender(students)
+console.log(qualificationList(students, califications));
 
 
-const qualificationsList = (qualifications) => {
-
-    const studentCalifications = {};
-    for (const calification of qualifications) {
-    const student = students.find((s) => s.id === calification.id);
-    if (student) {
-    if (!studentCalifications[student.id]) {
-      studentCalifications[student.id] = [];
-    }
-    studentCalifications[student.id].push(calification.calification);
-  }
-}
-console.log('Qualifications:', studentCalifications);
-}
-
-qualificationsList(califications)
-
-
+    
